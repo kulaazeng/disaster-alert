01af44a8b6a33e819003a7e93f121816dfa84e7b
kulaazeng <kittipong.aimtech@gmail.com>
2025-05-23 11:18:24 +0700

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RegionsModule } from './regions/regions.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseConfigAsync } from './common/configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    envFilePath: '.env',
-  }), RegionsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync(DatabaseConfigAsync),
+    RegionsModule,
+  ],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/common/configs/database.config.ts
@@ -0,0 +1,28 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+    TypeOrmModuleAsyncOptions,
+    TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+
+export class DatabaseConfig {
+    static getConfig(configService: ConfigService): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: configService.getOrThrow<string>('DATABASE_HOST'),
+            port: configService.getOrThrow<number>('DATABASE_PORT'),
+            username: configService.getOrThrow<string>('DATABASE_USER'),
+            password: configService.getOrThrow<string>('DATABASE_PASSWORD'),
+            database: configService.getOrThrow<string>('DATABASE_NAME'),
+            entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+            synchronize: true,
+            logging: true,
+        };
+    }
+}
+
+export const DatabaseConfigAsync: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService): TypeOrmModuleOptions =>
+        DatabaseConfig.getConfig(configService),
+    inject: [ConfigService],
+};
