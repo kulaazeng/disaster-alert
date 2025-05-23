d4bb2cf5ba5136c9373751ad40119c9404ed4354
kulaazeng <kittipong.aimtech@gmail.com>
2025-05-23 11:34:41 +0700

    create table regions

---
+++ src/regions/entities/region.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+
+@Entity('regions')
+export class Region {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  regionId: string;
+
+  @Column('simple-array')
+  disasterTypes: string[];
+
+  @Column('decimal')
+  latitude: number;
+
+  @Column('decimal')
+  longitude: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
